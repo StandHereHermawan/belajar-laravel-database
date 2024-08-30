48e1bc42ba147bd96227a8eccaa3bf80ffeb7d1c
Arief-Karditya <nolifegansaya@gmail.com>
2024-08-30 13:39:10 +0700

    Query Builder Insert

---
+++ tests/Feature/QueryBuilder/InsertTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tests\Feature\QueryBuilder;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\DB;
+use Tests\TestCase;
+
+class InsertTest extends TestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+        DB::delete("DELETE FROM categories");
+    }
+
+    public function test_example(): void
+    {
+        DB::table("categories")->insert([
+            'id' => 'GADGET',
+            'name' => 'Gadget'
+        ]);
+        DB::table("categories")->insert([
+            'id' => 'FOOD',
+            'name' => 'Food'
+        ]);
+        DB::table("categories")->insert([
+            'id' => 'SPARE-PART',
+            'name' => 'Sparepart'
+        ]);
+
+        $result = DB::select("SELECT COUNT(id) as total FROM categories");
+
+        self::assertNotNull($result);
+        self::assertEquals(3, $result[0]->total);
+    }
+}
